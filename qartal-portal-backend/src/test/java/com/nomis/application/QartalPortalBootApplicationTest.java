1ae03c25977986a93a5a19dee59c309e519d25f2
LeshaRB <lesharb@gmail.com>
2018-04-12 19:21:09 +0300

    Gradle

---
+++ qartal-portal-backend/src/test/java/com/nomis/application/QartalPortalBootApplicationTest.java
@@ -0,0 +1,15 @@
+package com.nomis.application;
+
+/**
+ * QartalPortalBootApplicationTest.
+ *
+ * @author Aliaksei Labotski.
+ * @since 4/12/18.
+ */
+//@SpringBootConfiguration
+//@RunWith(SpringRunner.class)
+//@Slf4j
+@SuppressWarnings("PMD.UseUtilityClass")
+public class QartalPortalBootApplicationTest {
+
+}
